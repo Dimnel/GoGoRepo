767740aa7ddea5c209718fa70c5ffe2330ddb279
Dimnel <32062970+Dimnel@users.noreply.github.com>
2017-09-18 13:41:03 +0200

    Add files via upload

---
+++ RunningMouse/RunningMouse/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RunningMouse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Random los = new Random();
+            int x,y;
+            x = los.Next(button1.Width, 475 - button1.Width);
+            y = los.Next(button1.Height, 409 - button1.Height);
+
+            Point p = new Point(x, y);
+            button1.Location = p;
+        }
+    }
+}
